64f279d489d0b7623f5c55e4b0650eaa1fd0a9c9
Kevin Freyberg <kfreyberg01@gmail.com>
2022-09-19 12:53:05 -0400

    Update urbansound.py

--- urbansound.py
+++ urbansound.py
@@ -1,13 +1,69 @@
+import os
+from random import sample
+from tenacity import retry_unless_exception_type
+import torch
 from torch.utils.data import Dataset
 import pandas as pd
+import torchaudio
 
 class UrbanSoundDataset(Dataset):
 
-    def __init__(self, annotations_file, audio_dir):
+    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate):
         self.annotations = pd.read_csv(annotations_file)
+        self.audio_dir = audio_dir
+        self.transformation = transformation
+        self.target_sample_rate = target_sample_rate
 
     def __len__(self):
-        pass
+        return len(self.annotations)
 
     def __getitem__(self, index):
-        
+        audio_sample_path = self._get_audio_sample_path(index)
+        label = self._get_audio_sample_label(index)
+        signal, sr = torchaudio.load(audio_sample_path)
+        # signal -> (num_channels, samples) -> (2, 16000) - > (1, 16000)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self.transformation(signal) # ms = mel_spectrogram(signal)
+        return signal, label
+
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
+
+    def _mix_down_if_necessary(self, signal):
+        if signal.shape[0] > 1:
+            # if we have a signal with more then one channel, we want to mix it down to mono
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
+
+    def _get_audio_sample_path(self, index):
+        fold = f"fold{self.annotations.iloc[index, 5]}"
+        path = os.path.join(self.audio_dir, fold, self.annotations.iloc[index, 0])
+        return path
+
+    def _get_audio_sample_label(self, index):
+        return self.annotations.iloc[index, 6]
+
+
+if __name__ == "__main__":
+    ANNOTATIONS_FILE = ""
+    AUDIO_DIR = ""
+    SAMPLE_RATE = 16000 
+
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft = 1024,
+        hop_length = 512,
+        n_mels = 64
+    )
+
+
+    usd = UrbanSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE)
+
+    print(f"There are {len(usd)} samples in the dataset.")
+    signal, label = usd[0]
+    print(signal)
+
